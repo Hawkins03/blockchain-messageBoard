13468106a0a7dfd3bb7924521248cb3ef98ddc83
FA1C0Nl-lAWK <falcor.peterson@gmail.com>
2022-09-23 16:48:59 -0400

    read prev commit message, I messed them up

--- BlockChain.py
+++ BlockChain.py
@@ -2,6 +2,7 @@ import os
 import socket
 import json
 import Crypto
+import requests
 from time import time
 from urllib.parse import urlparse
 from Crypto.PublicKey import RSA
@@ -28,82 +29,7 @@ class BlockChain:
         self.chain = []
         self.nodes = set()
 
-        self.new_block(previous_hash='1', proof=100)
-
-    def new_block(self, message, proof, prev_hash = self.hash(self.chain[-1])):
-        block = {
-            'pubkey': self.pubkey,
-            'index': len(self.chain) + 1,
-            'timestamp': time(),
-            'message': b64encode(message),
-            'proof': proof,
-            'prev_hash': prev_hash,
-        }
-        self.chain.append(block)
-
-        return block
-    
-    def new_block(self, message):
-        if (self.chain.length == 0):
-            return
-        
-        prev_hash = self.hash(self.chain[-1])
-        proof = proof_of_work(self.cipher[-1])
-
-        block = {
-            'pubkey': self.pubkey,
-            'index': len(self.chain) + 1,
-            'timestamp': time(),
-            'message': b64encode(message),
-            'proof': proof,
-            'prev_hash': prev_hash,
-            'signature': cipher.sign(SHA256.new(message))
-        }
-
-        self.chain.append(block)
-        return block
-
-    @property
-    def lastBlock(self):
-        return self.chain[-1]
-
-    def hash(self, block):
-        """
-        get the hash of a block
-        :param block: <dict> the block to collect a hash for
-        :return: <:class: SHA256Hash> the hash of the message
-        """
-        block_str = json.dumps(block, sort_keys=True)
-        return SHA256.new(block_str)
-
-    def proof_of_work(self, lastBlock):
-        """
-        returns a proof of work (an intger that when hashed with the hash of the last block and proof from the hashed block is signed)
-
-        :param lastBlock: <dict> the previous node in the BlockChain
-        :return: <int> the new valid proof of work
-        """
-
-        lastProof = lastBlock['proof']
-        lastHash = self.hash(lastBlock)
-
-        proof = 0
-        while (not self.validProof(lastProof, proof, lastHash)):
-            proof+=1
-
-        return proof
-    
-    @staticmethod
-    def validProof(lastProof, proof, lastHash):
-        '''
-        :param lastproof: <int> the proof of work from the previous block
-        :param proof: <int> the proof we're trying this time
-        :param lastHash: <str> the hash of the previous block
-        :return: <bool> true if the guess hash is signed (4 leading 0's)
-        '''
-        guess = f'{lastProof}{proof}{lastHash}'.encode()
-        guessHash = SHA1.new(guess).hexdigest()
-        return guessHash[:4] == "0000"
+        self.new_block(message="", previous_hash='1', proof=100)
 
     def register_node(self, address, pubkey):
         """
@@ -121,14 +47,85 @@ class BlockChain:
         else:
             raise ValueError('Invalid URL')
 
+    def new_block(self, message, proof = proof_of_work(self.cipher[-1]), prev_hash = self.hash(self.chain[-1])):
+        """
+        creates a new block and adds it to the chain (as well as gives it to you)
+
+        :param message: <str> the specific data the block holds
+        :param proof: <int> the number that allows the hash to be signed
+        :param prev_hash: <hash> the hash of the previous block in the chain
+        :return: <dict> the block to be added to the chain (note, it is added to the chain by the function as well)
+        """
+        block = {
+            'pubkey': self.pubkey,
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'message': b64encode(message),
+            'proof': proof,
+            'prev_hash': prev_hash,
+            'signature': cipher.sign(self.hash(b64encode(message)))
+        }
+        self.chain.append(block)
+
+        #push to all nodes : f"https://{node}/update"
+        return block
+
+    def pull_data(self, node):
+        """
+        :param node: <str> the node to request data from
+        :return: <bool> True if data was overwritten, false if otherwise
+        note, this will be called if an update gets pushed to {self}/update
+        """
+
+        response = requests.get(f"https://{node}/chain")
+
+        if (response.status_code == 200):
+            if (((response.json()['length'] > len(self.chain)))):
+                if (valid_chain(response.json()['chain'])):
+                    self.chain = response.json()[chain]
+                    return True
+        
+        return False
+
+    def resolve_conflicts(self):
+        """
+        resolves conflicts between the current blockchain and those owned by others
+
+        :return: <bool> True if the chain was replaced
+        """
+
+        neighbors = self.nodes
+        new_chain = None
+
+        max_length = len(self.chain)
+
+        #find largest node
+        for node in neighbors:
+            response = request.get(f"https://{node}/chain")
+
+            if (response.status_code == 200):
+                length = response.json()['length']
+                chain = response.json()['chain']
+
+                if (length > max_length):
+                    max_length = length
+                    new_chain = chain
+        
+        #Replace chain if a newer chain was discovered and it works out
+        if ((new_chain) and (valid_chain(new_chain))):
+            self.chain = new_chain
+            return True
+
+        return False
+
     def valid_chain(self, chain):
         """
         validates a new blockchain in 2 ways:
         for each block (beyond the first)
             it first checks that the hash matches
+
+        :param chain: <list> new blockchian to be checked
         """
-
-
         # counting backwards because it's more likely that a problem will be with anything new
         for i in range(len(chain), 1, -1):
             last_block = chain[i - 1]
@@ -151,5 +148,43 @@ class BlockChain:
                 return False
 
             last_block = block
-        
         return True
+    
+    @staticmethod
+    def validProof(lastProof, proof, lastHash):
+        '''
+        :param lastproof: <int> the proof of work from the previous block
+        :param proof: <int> the proof we're trying this time
+        :param lastHash: <str> the hash of the previous block
+        :return: <bool> true if the guess hash is signed (4 leading 0's)
+        '''
+        guess = f'{lastProof}{proof}{lastHash}'.encode()
+        guessHash = SHA1.new(guess).hexdigest()
+        return guessHash[:4] == "0000"
+
+
+    def proof_of_work(self, lastBlock):
+        """
+        returns a proof of work (an intger that when hashed with the hash of the last block and proof from the hashed block is signed)
+
+        :param lastBlock: <dict> the previous node in the BlockChain
+        :return: <int> the new valid proof of work
+        """
+
+        lastProof = lastBlock['proof']
+        lastHash = self.hash(lastBlock)
+
+        proof = 0
+        while (not self.validProof(lastProof, proof, lastHash)):
+            proof+=1
+
+        return proof
+    
+    def hash(self, block):
+        """
+        get the hash of a block
+        :param block: <dict> the block to collect a hash for
+        :return: <:class: SHA256Hash> the hash of the message
+        """
+        block_str = json.dumps(block, sort_keys=True)
+        return SHA256.new(block_str)
